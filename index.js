6e1590d2ccf91af5571ef18159cffaee7ef07af8
JoyRume <joy.rume@student.moringaschool.com>
2023-12-13 21:10:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+//const welcomeMessage ='welcome to flatbool,' ;
+//const welcomeMessage = currentUser;
+
+
+const welcomeMessage = `welcome to flatbook,  + ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
